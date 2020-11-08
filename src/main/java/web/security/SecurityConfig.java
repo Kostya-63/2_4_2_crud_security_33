2515f2968ad14925a4a1c761c9811ec78ac7a9a2
Kostya Petrov <KostyaPetrov1986@gmail.com>
2020-11-08 10:47:36 +0400

    Вернул страницу, имя юзера приходит в UserDetails

--- src/main/java/web/security/SecurityConfig.java
+++ src/main/java/web/security/SecurityConfig.java
@@ -34,11 +34,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.formLogin()
                 // указываем страницу с формой логина
-                .and().formLogin()
+                .loginPage("/login")
                 //указываем логику обработки при логине
                 .successHandler(successUserHandler)
                 // указываем action с формы логина
                 .loginProcessingUrl("/login")
+                // Указываем параметры логина и пароля с формы логина
+                .usernameParameter("j_username")
+                .passwordParameter("j_password")
                 // даем доступ к форме логина всем
                 .permitAll();
 
