f38f755076789f5ed8ced3e36f7d7c97999fc895
Kostya Petrov <KostyaPetrov1986@gmail.com>
2020-12-04 17:43:07 +0400

    пофиксил вкладки на странице админа

--- src/main/resources/static/getUser.js
+++ src/main/resources/static/getUser.js
@@ -5,8 +5,24 @@ function getUser() {
     fetch(url).then((res) => res.json())
         .then((user) => {
             let userRoles = "";
+            let output3 = "";
             for (let i = 0; i < user.rolesSet.length; i++) {
                 userRoles += `${user.rolesSet[i].role} `
+            }
+
+            if (userRoles.includes("ROLE_ADMIN")) {
+                output3 +=
+                    `<li class="nav-item">
+                        <a class="nav-link active" data-toggle="tab" href="#adminPanel">Admin</a>
+                    </li>`
+                ;
+            }
+            if (userRoles.includes("ROLE_USER")) {
+                output3 +=
+                    `<li class="nav-item">
+                        <a class="nav-link" data-toggle="tab" href="#userAdmin">User</a>
+                    </li>`
+                ;
             }
 
             let output = "<tr>";
@@ -24,6 +40,7 @@ function getUser() {
             let output2 = "";
             output2 += `${user.name}  with roles: ${user.roles}`;
             document.getElementById("header").innerHTML = output2;
+            document.getElementById("tabsAdminUser").innerHTML = output3;
         })
 }
 
